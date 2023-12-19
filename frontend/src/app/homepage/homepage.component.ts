a0efe5e9fb2812094d4634c831199e78ab3a86a5
NawrockiMateusz <mateusz.nawroo@gmail.com>
2023-12-19 22:26:47 +0100

    Change homepage view and add nav buttons

--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
+import { EventsComponent } from '../events/events.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [NavComponent],
+  imports: [NavComponent, EventsComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
 })
